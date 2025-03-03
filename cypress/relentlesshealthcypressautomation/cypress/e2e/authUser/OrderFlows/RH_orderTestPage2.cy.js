40d42dfe36bac85115b4b938501d92dc95c74de4
SamantaRubio <samanta.rubio@magmalabs.io>
2025-03-03 15:15:57 -0600

    order page 2
    page 2

--- cypress/relentlesshealthcypressautomation/cypress/e2e/authUser/OrderFlows/RH_orderTestPage2.cy.js
+++ cypress/relentlesshealthcypressautomation/cypress/e2e/authUser/OrderFlows/RH_orderTestPage2.cy.js
@@ -17,8 +17,15 @@ describe('Order a Test - Who is this test for?', () => {
 
     it('Empty Fields', function() {
         const { birthdate, emptyNameAlert, emptyBirthdateAlert } = this.orderTestPage2.Page2;
+        const { year, month, timestamp} = this.accountInformation.OrderTestData;
+
         cy.contains('button', 'Next').click(); 
         cy.get('.MuiInputBase-input.MuiOutlinedInput-input.css-m53ae2').clear();
+        cy.get('input[placeholder="DD/MM/YYYY"]').click();
+        cy.get('button[role="radio"][aria-checked="false"]').contains(year).click();
+        cy.get(`button[aria-label="${month}"]`).should('be.visible').click();
+        cy.contains('button', timestamp).should('be.visible').should('be.visible').click();
+        cy.contains('button', 'OK').should('be.visible').should('be.visible').click();
         // cy.get('input[placeholder="DD/MM/YYYY"]').type(birthdate); 
         // cy.get('input[placeholder="DD/MM/YYYY"]').clear(); 
         cy.get('input.PrivateSwitchBase-input[name="subjects[0].isAnAdult"]').uncheck();
@@ -32,7 +39,7 @@ describe('Order a Test - Who is this test for?', () => {
 
     it('Invalid Full Name', function() {
         const { birthdate, invalidName, invalidNameAlert } = this.orderTestPage2.Page2;
-        const { year, month, timestamp} = accountInformation.OrderTestData;
+        const { year, month, timestamp} = this.accountInformation.OrderTestData;
 
         cy.contains('button', 'Next').click(); 
         cy.get('.MuiInputBase-input.MuiOutlinedInput-input.css-m53ae2').clear().type(invalidName);
@@ -50,7 +57,7 @@ describe('Order a Test - Who is this test for?', () => {
 
     it('Invalid Birth date', function() {
         const { invalidBirthdate, invalidBirthdateAlert } = this.orderTestPage2.Page2;
-        const { month, timestamp} = accountInformation.OrderTestData;
+        const { month, timestamp} = this.accountInformation.OrderTestData;
         cy.contains('button', 'Next').click(); 
 
         cy.get('input[placeholder="DD/MM/YYYY"]').click();
